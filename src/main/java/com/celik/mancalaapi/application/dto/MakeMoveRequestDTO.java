3000bb26417f4dfada9aff2e311fe653c8f04910
Orcun Celik <orcun.celik@ykteknoloji.com.tr>
2023-04-07 00:26:10 +0300

    Refactored and added controller tests, changed MakeMoveRequestDTO according to tests

--- src/main/java/com/celik/mancalaapi/application/dto/MakeMoveRequestDTO.java
+++ src/main/java/com/celik/mancalaapi/application/dto/MakeMoveRequestDTO.java
@@ -1,5 +1,7 @@
 package com.celik.mancalaapi.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 import javax.validation.constraints.Max;
@@ -7,7 +9,13 @@ import javax.validation.constraints.Min;
 
 @Getter
 public class MakeMoveRequestDTO {
+
     @Min(value = 0, message = "Pit cannot be negative")
     @Max(13)
-    private int pitIndex;
+    private final int pitIndex;
+
+    @JsonCreator
+    public MakeMoveRequestDTO(@JsonProperty("pitIndex") int pitIndex) {
+        this.pitIndex = pitIndex;
+    }
 }
--- src/test/java/com/celik/mancalaapi/controller/MancalaGameControllerTest.java
+++ src/test/java/com/celik/mancalaapi/controller/MancalaGameControllerTest.java
@@ -1,11 +1,13 @@
 package com.celik.mancalaapi.controller;
 
+import com.celik.mancalaapi.application.dto.MakeMoveRequestDTO;
 import com.celik.mancalaapi.application.dto.MancalaGameStateDTO;
 import com.celik.mancalaapi.application.rest.controller.MancalaGameController;
+import com.celik.mancalaapi.domain.model.MancalaGame;
 import com.celik.mancalaapi.domain.model.MancalaGameState;
-import com.celik.mancalaapi.domain.model.enums.MancalaGameStatus;
-import com.celik.mancalaapi.domain.model.enums.MancalaPlayerType;
 import com.celik.mancalaapi.domain.service.MancalaGameService;
+import com.celik.mancalaapi.infrastructure.mapper.MancalaGameMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,49 +15,66 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import java.util.Arrays;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @WebMvcTest(MancalaGameController.class)
 class MancalaGameControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
+
     @MockBean
     private MancalaGameService gameService;
+
     private MancalaGameState gameState;
+
     private MancalaGameStateDTO gameStateDTO;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
-        gameState = new MancalaGameState(UUID.randomUUID(), Arrays.asList(6, 6, 6, 6, 6, 6), 0, Arrays.asList(6, 6, 6, 6, 6, 6), 0, MancalaGameStatus.CONTINUING, MancalaPlayerType.FIRST_PLAYER);
-        gameStateDTO = new MancalaGameStateDTO(UUID.randomUUID(), Arrays.asList(6, 6, 6, 6, 6, 6), 0, Arrays.asList(6, 6, 6, 6, 6, 6), 0, MancalaGameStatus.CONTINUING, MancalaPlayerType.FIRST_PLAYER);
+        MancalaGame game = new MancalaGame();
+        gameState = game.toGameState();
+        gameStateDTO = MancalaGameMapper.mapGameStateToDTO(gameState);
     }
 
     @Test
-    void testCreateGame() throws Exception {
+    void givenService_whenCreateGame_thenGameCreatedSuccessfully() throws Exception {
         when(gameService.createGame()).thenReturn(gameState);
-
         mockMvc.perform(post("/api/mancala")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.gameId").value("1"));
+                .andExpect(status().isCreated());
     }
 
     @Test
-    void testPlayGame() {
-        int pitId = 1;
-        String gameId = "1";
-
-        //doReturn(gameState)
-        //        .when(gameService)
-        //        .makeMove(null,1);
-
+    void givenService_whenGetGameState_thenGameStateGot() throws Exception {
+        UUID gameId = UUID.randomUUID();
+        when(gameService.getGameState(gameId)).thenReturn(gameState);
+        mockMvc.perform(get("/api/mancala/" + gameId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
+
+    @Test
+    void givenService_whenMakeMove_thenMakeMoveSuccessful() throws Exception {
+        UUID gameId = gameState.getGameId();
+        MakeMoveRequestDTO makeMoveRequestDTO = new MakeMoveRequestDTO(4);
+        when(gameService.getGameState(gameId)).thenReturn(gameState);
+        doNothing().when(gameService).makeMove(gameId, makeMoveRequestDTO.getPitIndex());
+        mockMvc.perform(put("/api/mancala/" + gameId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(objectMapper.writeValueAsString(makeMoveRequestDTO)))
+                .andExpect(status().isOk());
+    }
+
 }
